d5af740ac33168e103badfa1c45ead22272190ab
Marko Andrushchenko <markello123456789101@gmail.com>
2025-11-02 01:47:21 +0100

    name centered, part-created textarea tasks, not for safari

--- src/script.js
+++ src/script.js
@@ -3,129 +3,132 @@ const addBtn = document.querySelector('#add-task-btn');
 const tasksHolder = document.querySelector('.tasks-list');
 
 window.addEventListener('load', () => {
-  todos = JSON.parse(localStorage.getItem('todos')) || [];
-  nameInput = document.querySelector('#name');
-  const newTodoForm = document.querySelector('.main-header');
+	todos = JSON.parse(localStorage.getItem('todos')) || [];
+	nameInput = document.querySelector('#name');
+	const newTodoForm = document.querySelector('.main-header');
 
-  const userName = localStorage.getItem('userName') || '';
+	const userName = localStorage.getItem('userName') || '';
 
-  nameInput.value = userName;
+	nameInput.value = userName;
 
-  nameInput.addEventListener('input', (e) => {
-    localStorage.setItem('userName', e.target.value.trim());
-    addTasks();
-  });
+	nameInput.addEventListener('input', (e) => {
+		localStorage.setItem('userName', e.target.value.trim());
+		addTasks();
+	});
 
-  newTodoForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    // Rerturn whether input is empty
-    if (userInput.value.trim() === '') return;
+	newTodoForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+		// Rerturn whether input is empty
+		if (userInput.value.trim() === '') return;
 
-    const todo = {
-      owner: nameInput.value.trim(),
-      content: e.target.elements.content.value.trim(),
-      completed: false,
-      createdAt: new Date().getTime(),
-    };
+		const todo = {
+			owner: nameInput.value.trim(),
+			content: e.target.elements.content.value.trim(),
+			completed: false,
+			createdAt: new Date().getTime(),
+		};
 
-    todos.push(todo);
+		todos.push(todo);
 
-    localStorage.setItem('todos', JSON.stringify(todos));
+		localStorage.setItem('todos', JSON.stringify(todos));
 
-    e.target.reset();
+		e.target.reset();
 
-    addTasks();
-    userInput.value = '';
-    scrollDown(tasksHolder);
-  });
-  addTasks();
+		addTasks();
+		userInput.value = '';
+		scrollDown(tasksHolder);
+	});
+	addTasks();
 });
 
 function addTasks() {
-  tasksHolder.innerHTML = '';
+	tasksHolder.innerHTML = '';
 
-  todos.forEach((todo) => {
-    // Show only tasks for the specific onwers name
-    if (todo.owner !== nameInput.value.trim()) return;
+	todos.forEach((todo) => {
+		// Show only tasks for the specific onwers name
+		if (todo.owner !== nameInput.value.trim()) return;
 
-    const task = document.createElement('div');
-    const checkbox = task.appendChild(document.createElement('input'));
-    const text = task.appendChild(document.createElement('input'));
-    const editBtn = task.appendChild(document.createElement('button'));
-    const deleteBtn = task.appendChild(document.createElement('button'));
+		const task = document.createElement('div');
+		const checkbox = task.appendChild(document.createElement('input'));
+		const text = task.appendChild(document.createElement('textarea'));
+		const editBtn = task.appendChild(document.createElement('button'));
+		const deleteBtn = task.appendChild(document.createElement('button'));
 
-    task.className = 'task';
+		task.className = 'task';
 
-    // Checkbox initialization
-    checkbox.type = 'checkbox';
-    checkbox.checked = todo.completed;
+		// Checkbox initialization
+		checkbox.type = 'checkbox';
+		checkbox.checked = todo.completed;
 
-    // Text initializattion
-    text.className = 'task-content';
-    text.type = 'text';
-    text.readOnly = true;
-    text.value = todo.content;
+		// Text initializattion
+		text.className = 'task-content';
+		text.type = 'text';
+		text.readOnly = true;
+		text.value = todo.content;
 
-    // Edit btn initialization
-    editBtn.type = 'button';
-    editBtn.className = 'btn edit-btn material-symbols-outlined';
-    editBtn.innerText = 'edit';
-    initEditBtn(editBtn, text, todo);
+		const lines = text.value.split('\n').length || 1;
+		text.rows = lines;
 
-    // Delete btn initialization
-    deleteBtn.type = 'button';
-    deleteBtn.className = 'btn delete-btn material-symbols-rounded';
-    deleteBtn.innerText = 'close';
-    initDeleteBtn(deleteBtn, todo);
+		// Edit btn initialization
+		editBtn.type = 'button';
+		editBtn.className = 'btn edit-btn material-symbols-outlined';
+		editBtn.innerText = 'edit';
+		initEditBtn(editBtn, text, todo);
 
-    // Final changes to the list
-    tasksHolder.style.visibility = 'visible'; // Later on count tasks
-    tasksHolder.appendChild(task);
+		// Delete btn initialization
+		deleteBtn.type = 'button';
+		deleteBtn.className = 'btn delete-btn material-symbols-rounded';
+		deleteBtn.innerText = 'close';
+		initDeleteBtn(deleteBtn, todo);
 
-    // Check for completeness
-    if (todo.completed) {
-      text.classList.toggle('completed');
-    }
-    // Change completed tasks in localStorage
-    checkbox.addEventListener('change', (e) => {
-      todo.completed = e.target.checked;
-      localStorage.setItem('todos', JSON.stringify(todos));
-      // text.classList.toggle('completed');
-      if (todo.completed) {
-        text.classList.add('completed');
-      } else {
-        text.classList.remove('completed');
-      }
+		// Final changes to the list
+		tasksHolder.style.visibility = 'visible'; // Later on count tasks
+		tasksHolder.appendChild(task);
 
-      addTasks();
-    });
-  });
+		// Check for completeness
+		if (todo.completed) {
+			text.classList.toggle('completed');
+		}
+		// Change completed tasks in localStorage
+		checkbox.addEventListener('change', (e) => {
+			todo.completed = e.target.checked;
+			localStorage.setItem('todos', JSON.stringify(todos));
+			// text.classList.toggle('completed');
+			if (todo.completed) {
+				text.classList.add('completed');
+			} else {
+				text.classList.remove('completed');
+			}
+
+			addTasks();
+		});
+	});
 }
 
 const initEditBtn = (editBtn, text, todo) => {
-  editBtn.onclick = () => {
-    text.removeAttribute('readonly');
-    text.focus();
-    text.addEventListener('blur', (e) => {
-      text.setAttribute('readonly', true);
-      todo.content = text.value.trim();
-      localStorage.setItem('todos', JSON.stringify(todos));
-      addTasks();
-    });
-  };
+	editBtn.onclick = () => {
+		text.removeAttribute('readonly');
+		text.focus();
+		text.addEventListener('blur', (e) => {
+			text.setAttribute('readonly', true);
+			todo.content = text.value.trim();
+			localStorage.setItem('todos', JSON.stringify(todos));
+			addTasks();
+		});
+	};
 };
 
 const initDeleteBtn = (deleteBtn, todo) => {
-  deleteBtn.onclick = () => {
-    todos = todos.filter((t) => t != todo);
-    localStorage.setItem('todos', JSON.stringify(todos));
-    deleteBtn.parentNode.remove();
-  };
+	deleteBtn.onclick = () => {
+		todos = todos.filter((t) => t != todo);
+		localStorage.setItem('todos', JSON.stringify(todos));
+		deleteBtn.parentNode.remove();
+	};
 };
 
 const scrollDown = (tasksHolder) => {
-  tasksHolder.scrollTo({
-    top: tasksHolder.scrollHeight,
-    behavior: 'smooth',
-  });
+	tasksHolder.scrollTo({
+		top: tasksHolder.scrollHeight,
+		behavior: 'smooth',
+	});
 };
